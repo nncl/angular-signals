8711f43095be8ce2ec7018b7eae17007153efab0
Caue Almeida <caue.almeida@telefonicabigdata.onmicrosoft.com>
2024-08-26 15:50:33 -0300

    feat: :art: Sets dynamic multiplier conditionally
    We're creating the derivedCounter result depending on the multiplier, which its value is set when we press the Set to 10 button.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal, untracked } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -11,7 +11,15 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   counter = signal(0);
 
-  derivedCounter = computed(() => untracked(this.counter) * 10);
+  multiplier = 0;
+
+  derivedCounter = computed(() => {
+    if (this.counter() === 0) {
+      return 0;
+    } else {
+      return this.counter() * this.multiplier;
+    }
+  });
 
   constructor() {
     console.log(`counter value: ${this.counter()}`);
